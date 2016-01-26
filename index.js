982c4d7a282180db78adf075ac5204185c72a1a8
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-01-26 12:05:52 +0000

    Add semicolons where needed

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ var handler = webhookHandler({ path: "/deploy", secret: "james" });
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
+    res.statusCode = 404;
+    res.end('no such location');
   });
 }).listen(1996, function() {
   console.log("listening on 1996");
 });
 
 handler.on('error', function (err) {
-  console.error('Error:', err.message)
+  console.error('Error:', err.message);
 });
 
 handler.on('push', function (event) {
