d4a42a90429acdbcdc5592f91f097b055c4cb922
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-01-26 17:40:12 +0000

    Document pull.sh

--- pull.sh
+++ pull.sh
@@ -2,7 +2,14 @@
 
 echo "Pulling from repo..."
 
+# Change directory to ../wdots-website
 cd ../wdots-website
+
+# Stop the wdots-website pm2 service
 pm2 stop wdots-website
+
+# Pull the changes from git
 git pull
+
+# Start index.js with the name wdots-website
 pm2 start index.js --name wdots-website
