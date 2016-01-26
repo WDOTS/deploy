5f9b6fbf6324f084eab9e7e950e57e3863c05b87
James Monger <jameskmonger@hotmail.co.uk>
2016-01-26 10:54:55 +0000

    Listen with handler

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var webhookHandler = require('github-webhook-handler');
 var handler = webhookHandler({ path: "/deploy", secret: "james" });
 
 http.createServer(function (req, res) {
-  res.statusCode = 200;
-  res.end("hello world");
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  });
 }).listen(1996, function() {
   console.log("listening on 1996");
 });
