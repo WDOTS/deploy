da6c0ac8d389400c9723462de49e9321c41ef487
James Monger <jameskmonger@hotmail.co.uk>
2016-01-26 10:54:00 +0000

    Start handler

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var http = require('http');
+var webhookHandler = require('github-webhook-handler');
+var handler = webhookHandler({ path: "/deploy", secret: "james" });
 
 http.createServer(function (req, res) {
   res.statusCode = 200;
